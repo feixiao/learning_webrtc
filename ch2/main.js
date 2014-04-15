479af5c77f2f8c7acd2401e8c2ca45387ef86fd4
dristic <dristic101@gmail.com>
2014-04-14 18:01:41 -0700

    Add chapter 2 examples.

--- ch2/main.js
+++ ch2/main.js
@@ -1,6 +1,28 @@
-console.log("Hello");
+function hasUserMedia() {
+  return !!(navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+}
 
-navigator.webkitGetUserMedia({ video: true, audio: true }, function (stream) {
-  var video = document.querySelector('video');
-  video.src = window.URL.createObjectURL(stream);
-});
+if (hasUserMedia()) {
+  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+  navigator.getUserMedia({
+    video: {
+      mandatory: {
+        maxWidth: 320,
+        maxHeight: 480
+      },
+      optional: [
+        { maxWidth: 640 },
+        { maxHeigth: 480 }
+      ]
+    },
+    audio: false
+  }, function (stream) {
+    var video = document.querySelector('video');
+    video.src = window.URL.createObjectURL(stream);
+  }, function (error) {
+    console.log("Raised an error when capturing:", error);
+  });
+} else {
+  alert("Sorry, your browser does not support getUserMedia.");
+}
---
+++ ch2/photobooth.js
@@ -0,0 +1,39 @@
+function hasUserMedia() {
+  return !!(navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+}
+
+if (hasUserMedia()) {
+  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+  var video = document.querySelector('video'),
+      canvas = document.querySelector('canvas'),
+      streaming = false;
+
+  navigator.getUserMedia({
+    video: true,
+    audio: false
+  }, function (stream) {
+    video.src = window.URL.createObjectURL(stream);
+    streaming = true;
+  }, function (error) {
+    console.log("Raised an error when capturing:", error);
+  });
+
+  var filters = ['', 'grayscale', 'sepia'],
+      currentFilter = 0;
+  document.querySelector('video').addEventListener('click', function (event) {
+    if (streaming) {
+      canvas.width = video.clientWidth;
+      canvas.height = video.clientHeight;
+
+      var context = canvas.getContext('2d');
+      context.drawImage(video, 0, 0);
+
+      currentFilter++;
+      if(currentFilter > filters.length - 1) currentFilter = 0;
+      canvas.className = filters[currentFilter];
+    }
+  });
+} else {
+  alert("Sorry, your browser does not support getUserMedia.");
+}
