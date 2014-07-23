7a2a777b5308b4671b19628a202f2ef57aa87d44
dristic <dristic101@gmail.com>
2014-07-23 00:16:18 -0700

    Add more functionality to example.

--- ch4/client.js
+++ ch4/client.js
@@ -1,13 +1,48 @@
 var connection = new WebSocket('ws://localhost:8888');
 
+var name = "User" + Math.floor(Math.random() * 100);
+
 connection.onopen = function () {
   console.log("Connected");
+
+  connection.send(JSON.stringify({
+    type: "login",
+    name: name
+  }));
 };
 
 connection.onmessage = function (message) {
   console.log("Got message", message.data);
+
+  var data = JSON.parse(message.data);
+
+  switch(data.type) {
+    case "login":
+      onLogin(data.success);
+      break;
+    default:
+      break;
+  }
 };
 
 connection.onerror = function (err) {
   console.log("Got error", err);
 };
+
+var timeout = 500;
+function onLogin(success) {
+  if (success === false) {
+    // Try to login again after a timeout
+    setTimeout(function () {
+        var name = "User" + Math.floor(Math.random() * 100);
+        connection.send(JSON.stringify({
+          type: "login",
+          name: name
+        }));
+    }, timeout);
+    timout = timeout * 2;
+  } else {
+    timeout = 500;
+    console.log("READY!");
+  }
+}
--- ch4/index.js
+++ ch4/index.js
@@ -1,12 +1,99 @@
 var WebSocketServer = require('ws').Server,
-    wss = new WebSocketServer({ port: 8888 });
+    wss = new WebSocketServer({ port: 8888 }),
+    users = {};
 
 wss.on('connection', function (connection) {
   console.log("User connected");
 
-  wss.on('message', function (message) {
+  connection.on('message', function (message) {
     console.log("Got message:", message);
+
+    var data;
+
+    try {
+      data = JSON.parse(message);
+    } catch (e) {
+      console.log("Error parsing JSON");
+      data = {};
+    }
+
+    switch (data.type) {
+      case "login":
+        console.log("User logged in as", data.name);
+        if (users[data.name]) {
+          sendTo(connection, {
+            type: "login",
+            success: false
+          });
+        } else {
+          users[data.name] = connection;
+          sendTo(connection, {
+            type: "login",
+            success: true
+          });
+        }
+
+        break;
+      case "offer":
+        console.log("Sending offer to", data.name);
+        var conn = users[data.name];
+
+        if (conn != null) {
+          sendTo(conn, {
+            type: "offer",
+            offer: data.offer
+          });
+        }
+
+        break;
+      case "answer":
+        console.log("Sending answer to", data.name);
+        var conn = users[data.name];
+
+        if (conn != null) {
+          sendTo(conn, {
+            type: "answer",
+            answer: data.answer
+          });
+        }
+
+        break;
+      case "candidate":
+        console.log("Sending candidate to", data.name);
+        var conn = users[data.name];
+
+        if (conn != null) {
+          sendTo(conn, {
+            type: "candidate",
+            candidate: data.candidate
+          });
+        }
+
+        break;
+      case "leave":
+        console.log("Disconnecting user from", data.name);
+        var conn = users[data.name];
+
+        if (conn != null) {
+          sendTo(conn, {
+            type: "leave"
+          });
+        }
+
+        break;
+      default:
+        sendTo(connection, {
+          type: "error",
+          message: "Unrecognized command: " + data.type
+        });
+
+        break;
+    }
   });
 
-  connection.send('Hello World');
+  sendTo(connection, "Hello World");
 });
+
+function sendTo(conn, message) {
+  conn.send(JSON.stringify(message));
+}
