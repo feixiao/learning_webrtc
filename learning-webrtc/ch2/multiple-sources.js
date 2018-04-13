62439a3c640061ab0bd7769cbb2a1b75727f207f
feixiao <feixiao2020@sina.com>
2018-04-13 13:59:10 +0800

    Select camera, microphone and speaker

--- learning-webrtc/ch2/multiple-sources.js
+++ learning-webrtc/ch2/multiple-sources.js
@@ -1,44 +1,109 @@
-function hasUserMedia() {
-  return !!(navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+'use strict';
+
+var videoElement = document.querySelector('video');
+var audioInputSelect = document.querySelector('select#audioSource');
+var audioOutputSelect = document.querySelector('select#audioOutput');
+var videoSelect = document.querySelector('select#videoSource');
+var selectors = [audioInputSelect, audioOutputSelect, videoSelect];
+
+audioOutputSelect.disabled = !('sinkId' in HTMLMediaElement.prototype);
+
+function gotDevices(deviceInfos) {
+  // Handles being called several times to update labels. Preserve values.
+  var values = selectors.map(function(select) {
+    return select.value;
+  });
+  selectors.forEach(function(select) {
+    while (select.firstChild) {
+      select.removeChild(select.firstChild);
+    }
+  });
+  for (var i = 0; i !== deviceInfos.length; ++i) {
+    var deviceInfo = deviceInfos[i];
+    var option = document.createElement('option');
+    option.value = deviceInfo.deviceId;
+    if (deviceInfo.kind === 'audioinput') {
+      option.text = deviceInfo.label ||
+          'microphone ' + (audioInputSelect.length + 1);
+      audioInputSelect.appendChild(option);
+    } else if (deviceInfo.kind === 'audiooutput') {
+      option.text = deviceInfo.label || 'speaker ' +
+          (audioOutputSelect.length + 1);
+      audioOutputSelect.appendChild(option);
+    } else if (deviceInfo.kind === 'videoinput') {
+      option.text = deviceInfo.label || 'camera ' + (videoSelect.length + 1);
+      videoSelect.appendChild(option);
+    } else {
+      console.log('Some other kind of source/device: ', deviceInfo);
+    }
+  }
+  selectors.forEach(function(select, selectorIndex) {
+    if (Array.prototype.slice.call(select.childNodes).some(function(n) {
+      return n.value === values[selectorIndex];
+    })) {
+      select.value = values[selectorIndex];
+    }
+  });
 }
 
-if (hasUserMedia()) {
-  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+navigator.mediaDevices.enumerateDevices().then(gotDevices).catch(handleError);
 
-  MediaStreamTrack.getSources(function(sources) {
-    var audioSource = null;
-    var videoSource = null;
-
-    for (var i = 0; i != sources.length; ++i) {
-      var source = sources[i];
-
-      if(source.kind === "audio") {
-        console.log("Microphone found:", source.label, source.id);
-        audioSource = source.id;
-      } else if (source.kind === "video") {
-        console.log("Camera found:", source.label, source.id);
-        videoSource = source.id;
-      } else {
-        console.log("Unknown source found:", source);
+// Attach audio output device to video element using device/sink ID.
+function attachSinkId(element, sinkId) {
+  if (typeof element.sinkId !== 'undefined') {
+    element.setSinkId(sinkId)
+    .then(function() {
+      console.log('Success, audio output device attached: ' + sinkId);
+    })
+    .catch(function(error) {
+      var errorMessage = error;
+      if (error.name === 'SecurityError') {
+        errorMessage = 'You need to use HTTPS for selecting audio output ' +
+            'device: ' + error;
       }
-    }
-
-    var constraints = {
-      audio: {
-        optional: [{sourceId: audioSource}]
-      },
-      video: {
-        optional: [{sourceId: videoSource}]
-      }
-    };
-
-    navigator.getUserMedia(constraints, function (stream) {
-      var video = document.querySelector('video');
-      video.src = window.URL.createObjectURL(stream);
-    }, function (error) {
-      console.log("Raised an error when capturing:", error);
+      console.error(errorMessage);
+      // Jump back to first output device in the list as it's the default.
+      audioOutputSelect.selectedIndex = 0;
     });
-  });
-} else {
-  alert("Sorry, your browser does not support getUserMedia.");
+  } else {
+    console.warn('Browser does not support output device selection.');
+  }
+}
+
+function changeAudioDestination() {
+  var audioDestination = audioOutputSelect.value;
+  attachSinkId(videoElement, audioDestination);
+}
+
+function gotStream(stream) {
+  window.stream = stream; // make stream available to console
+  videoElement.srcObject = stream;
+  // Refresh button list in case labels have become available
+  return navigator.mediaDevices.enumerateDevices();
+}
+
+function start() {
+  if (window.stream) {
+    window.stream.getTracks().forEach(function(track) {
+      track.stop();
+    });
+  }
+  var audioSource = audioInputSelect.value;
+  var videoSource = videoSelect.value;
+  var constraints = {
+    audio: {deviceId: audioSource ? {exact: audioSource} : undefined},
+    video: {deviceId: videoSource ? {exact: videoSource} : undefined}
+  };
+  navigator.mediaDevices.getUserMedia(constraints).
+      then(gotStream).then(gotDevices).catch(handleError);
+}
+
+audioInputSelect.onchange = start;
+audioOutputSelect.onchange = changeAudioDestination;
+videoSelect.onchange = start;
+
+start();
+
+function handleError(error) {
+  console.log('navigator.getUserMedia error: ', error);
 }
